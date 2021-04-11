0f7e2cfac6805f0de47d0c9cef6d4ae05ae215b4
kanto <watanabe.kanto@gmail.com>
2021-04-11 11:44:17 +0900

    設定情報追加

--- config/settings.php
+++ config/settings.php
@@ -1,8 +1,15 @@
 <?php
 use Monolog\Logger;
 
+error_reporting(0);
+ini_set('display_errors', '0');
+
+date_default_timezone_set('Asia/Tokyo');
+
 $settings = [
     'settings' => [
+        'environment' => 'default',
+        'debug' => false,
         'view' => [
             'path' => __DIR__ . '/../templates',
             'cache' => __DIR__ . '/../tmp/cache/twig',
