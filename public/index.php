354437c0a2523894c01fa34120024f2c78293944
kanto <watanabe.kanto@gmail.com>
2021-04-18 14:12:40 +0900

    ハンドラー追加

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
+use Slim\Factory\ServerRequestCreatorFactory;
+use App\Handlers\HttpErrorHandler;
+use App\Handlers\ShutdownHandler;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -14,5 +17,20 @@ $app = AppFactory::create();
 
 $routes = require __DIR__ . '/../config/routes.php';
 $routes($app);
+
+$callableResolver = $app->getCallableResolver();
+$responseFactory = $app->getResponseFactory();
+
+$serverRequestCreator = ServerRequestCreatorFactory::create();
+$request = $serverRequestCreator->createServerRequestFromGlobals();
+
+$displayErrorDetails = $container->get('settings')['debug'];
+
+$errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
+$shutdownHandler = new ShutdownHandler($request, $errorHandler, $displayErrorDetails);
+register_shutdown_function($shutdownHandler);
+
+$errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, false, false);
+$errorMiddleware->setDefaultErrorHandler($errorHandler);
 
 $app->run();
