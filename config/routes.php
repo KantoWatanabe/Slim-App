18483dec3e686956074a4c56a432a18c7fca2a1f
kanto <watanabe.kanto@gmail.com>
2021-04-18 15:24:21 +0900

    ミドルウェア追加

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,10 @@
 <?php
 use Slim\App;
+
+use App\Middleware\ExampleMiddleware;
 use App\Actions\HomeAction;
 
 return function (App $app) {
 
-    $app->get('/', HomeAction::class);
+    $app->get('/', HomeAction::class)->add(ExampleMiddleware::class);
 };
---
+++ src/Middleware/ExampleMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Middleware;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface as Middleware;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Container\ContainerInterface;
+
+class ExampleMiddleware implements Middleware
+{
+    /**
+     * @var ContainerInterface
+     */
+    private ContainerInterface $container;
+
+    /**
+     * ExampleMiddleware constructor.
+     *
+     * @param ContainerInterface  $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(Request $request, RequestHandler $handler): Response
+    {
+
+        return $handler->handle($request);
+    }
+}
